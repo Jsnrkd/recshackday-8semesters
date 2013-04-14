3a742529c8bdd54799e7112d40959d65de71edea
Jonathan Mayhak <jonathan@apparctica.com>
2013-04-14 11:14:50 -0400

    added dummy data

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -5,44 +5,44 @@ function getMajor(data) {
             description : 'The design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Physiological Psychology',
+            description : 'A subdivision of behavioral neuroscience (biological psychology) that studies the neural mechanisms of perception and behavior through direct manipulation of the brains of nonhuman animal subjects in controlled experiments.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Developmental Psychology',
+            description : 'The scientific study of changes that occur in human beings over the course of their life span. Originally concerned with infants and children, the field has expanded to include adolescence, adult development, aging, and the entire life span.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Industrial Psychology',
+            description : ' the scientific study of employees, workplaces, and organizations. Industrial and organizational psychologists contribute to an organization\'s success by improving the workplace and the performance, satisfaction and well-being of its people.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Criminal Science',
+            description : 'The study of crime in order to find ways to prevent it. Three features distinguish crime science from criminology: it is single-minded about cutting crime, rather than studying it for its own sake; accordingly it focuses on crime rather than criminals; and it is multidisciplinary, notably recruiting scientific methodology rather than relying on social theory.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Interior Design',
+            description : 'Describes a group of various yet related projects that involve turning an interior space into an "effective setting for the range of human activities" that are to take place there.[1] An interior designer is someone who conducts such projects.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Toxicology',
+            description : 'A branch of biology, chemistry, and medicine concerned with the study of the adverse effects of chemicals on living organisms.[1] It is the study of symptoms, mechanisms, treatments and detection of poisoning, especially the poisoning of people.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Ecology',
+            description : 'The scientific study of the relationships that living organisms have with each other and with their abiotic environment. Topics of interest to ecologists include the composition, distribution, amount (biomass), number, and changing states of organisms within and among ecosystems.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Biochemistry',
+            description : ' the study of chemical processes within, and relating to, living organisms.'
         },
         {
-            title : 'Landscape Architecture',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            title : 'Anatomy',
+            description : 'a branch of biology and medicine that considers the structure of living things. It is a general term that includes human anatomy, animal anatomy (zootomy), and plant anatomy (phytotomy).'
         },
         {
             title : 'Early Childhood Education',
-            description : ' the design of outdoor public areas, landmarks, and structures to achieve environmental, social-behavioral, or aesthetic outcomes. '
+            description : 'The formal teaching of young children by people outside the family or in settings outside the home.'
         },
         {
             title : 'Applied mathematics',
@@ -51,45 +51,14 @@ function getMajor(data) {
         {
             title : 'Home Economics',
             description : 'Home economics is a field of formal study including such topics as consumer education, institutional management, interior design, home furnishing, cleaning, handicrafts, sewing, clothing and textiles, commercial cooking, cooking, nutrition, food preservation, hygiene, child development, managing money, and family relationships. This teaches students how to properly run a family environment and make the world a better place for generations to come.'
-        },
-        '',
-        '',
-        'Early Childhood Education',
-        'Anatomy',
-        'Biochemistry',
-        'Ecology',
-        'Toxicology',
-        'Interior Design',
-        'Criminal Science',
-        'European History',
-        'Industrial Psychology',
-        'Development Psychology',
-        'Physiological Psychology'
+        }
     ];
-    $.ajax({
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
-    });
+
+    return data[Math.floor(Math.random() * data.length)];
 }
 
-function getColleges(major) {
-    $.ajax({
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
-    });
-}
+function getCollegesAndBooks(major) {
 
-function getBooks(major) {
-    $.ajax({
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
-    });
 }
 
 $(document).ready(function() {
