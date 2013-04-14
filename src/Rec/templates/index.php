5ebb0e9b1b8b154124f3c82903cb37bfabc92461
Jonathan Mayhak <jonathan@apparctica.com>
2013-04-14 12:29:34 -0400

    forgot response

--- src/Rec/templates/index.php
+++ src/Rec/templates/index.php
@@ -222,7 +222,7 @@
         }
     });
 
-    function getBooks() {
+    function getBooks(response) {
         for (var i = 0; i < response.items.length; i++) {
             var item = response.items[i];
             // in production code, item.text should have the HTML entities escaped.
